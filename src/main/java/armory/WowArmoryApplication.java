d0b31c6c120542dad1852d01375399a1c98d4b7e
Taewii <p3rfec7@abv.bg>
2019-08-27 20:06:20 +0300

    empty base project

---
+++ src/main/java/armory/WowArmoryApplication.java
@@ -0,0 +1,11 @@
+package armory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowArmoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WowArmoryApplication.class, args);
+    }
+}
