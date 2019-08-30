afa1e1a2b14aff07b23647242dd36b86d2fa778d
Taewii <p3rfec7@abv.bg>
2019-08-30 21:42:17 +0300

    JWT authorization/authentication configuration

--- src/main/java/armory/config/SecurityConfig.java
+++ src/main/java/armory/config/SecurityConfig.java
@@ -1,29 +1,68 @@
 package armory.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import armory.auth.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    public SecurityConfig(AuthenticationEntryPoint authenticationEntryPoint,
+                          JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
                 .cors()
-                    .disable()
+                    .and()
                 .csrf()
                     .disable()
+                .exceptionHandling()
+                    .authenticationEntryPoint(authenticationEntryPoint)
+                    .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                    .antMatchers("/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
                         .permitAll()
-                    .antMatchers("/account/create")
-                        .anonymous()
+                    .antMatchers("/api/auth/**")
+                        .permitAll()
+                    .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
+                        .permitAll()
                     .anyRequest()
                         .authenticated();
-
          // @formatter:on
     }
 }
--- src/main/java/armory/repositories/AccountRepository.java
+++ src/main/java/armory/repositories/AccountRepository.java
@@ -2,10 +2,24 @@ package armory.repositories;
 
 import armory.domain.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    @Query("SELECT a FROM Account a JOIN FETCH a.roles WHERE a.id = :id")
+    Optional<Account> findByIdWithRoles(@Param("id") UUID id);
+
+    Optional<Account> findByUsernameOrEmail(@NotBlank String username, @Email @NotBlank String email);
+
+    Boolean existsByUsername(@NotBlank String username);
+
+    Boolean existsByEmail(@Email @NotBlank String email);
 }
