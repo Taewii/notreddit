2b17a39012e6eb86b0efa84ef37239bf99a65e1a
Taewii <p3rfec7@abv.bg>
2019-09-27 21:02:32 +0300

    thumbnail service

---
+++ notreddit-server/src/main/java/notreddit/services/ThumbnailService.java
@@ -0,0 +1,13 @@
+package notreddit.services;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+public interface ThumbnailService {
+
+    String generateThumbnailUrl(String url);
+
+    String generateScreenshotApiUrl(Map<String, String> options) throws UnsupportedEncodingException;
+
+    String generatePdfApiUrl(Map<String, String> options) throws UnsupportedEncodingException;
+}
