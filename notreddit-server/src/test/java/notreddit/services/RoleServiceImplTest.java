899847370bd92d269bae7256105db160fb9dd94a
Taewii <p3rfec7@abv.bg>
2019-12-02 22:19:40 +0200

    - role service tests - updated dockerfile so maven can skip running the tests when its building

---
+++ notreddit-server/src/test/java/notreddit/services/RoleServiceImplTest.java
@@ -0,0 +1,62 @@
+package notreddit.services;
+
+import notreddit.domain.entities.Role;
+import notreddit.domain.enums.Authority;
+import notreddit.repositories.RoleRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+class RoleServiceImplTest {
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @InjectMocks
+    private RoleServiceImpl roleService;
+
+    private List<Role> getRoles() {
+        Role rootRole = new Role();
+        rootRole.setAuthority(Authority.ROOT);
+        Role adminRole = new Role();
+        adminRole.setAuthority(Authority.ADMIN);
+        Role moderatorRole = new Role();
+        moderatorRole.setAuthority(Authority.MODERATOR);
+        Role userRole = new Role();
+        userRole.setAuthority(Authority.USER);
+
+        return List.of(rootRole, adminRole, moderatorRole, userRole);
+    }
+
+    @Test
+    void getAllAsStrings_shouldWorkCorrectly() {
+        when(roleRepository.findAll()).thenReturn(this.getRoles());
+        List<String> result = roleService.getAllAsStrings();
+
+        assertEquals(4, result.size());
+        assertTrue(result.contains("ROOT"));
+        assertTrue(result.contains("ADMIN"));
+        assertTrue(result.contains("MODERATOR"));
+        assertTrue(result.contains("USER"));
+    }
+
+    @Test
+    void getAllAsStrings_withNoRoles_shouldReturnEmptyList() {
+        when(roleRepository.findAll()).thenReturn(new ArrayList<>());
+        List<String> result = roleService.getAllAsStrings();
+
+        assertTrue(result.isEmpty());
+    }
+}
