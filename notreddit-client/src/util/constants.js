31118521499fb5c536cf4f848ce304c77849649c
Taewii <p3rfec7@abv.bg>
2019-12-10 19:11:16 +0200

    changed the api url to the aws ebs one

--- notreddit-client/src/util/constants.js
+++ notreddit-client/src/util/constants.js
@@ -1,5 +1,6 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
 // export const API_BASE_URL = 'http://192.168.99.100:8001/api';
+export const API_BASE_URL = 'http://notreddit-env.fspzgqurca.us-east-1.elasticbeanstalk.com/api'
 export const ACCESS_TOKEN = 'accessToken';
 
 export const USERNAME_MIN_LENGTH = 4;
